6aa6d636b3b29fa5f1769d9b3542eed91b7bfa17
Atte Oksanen <37768995+Mixerboy24@users.noreply.github.com>
2019-12-19 16:22:18 +0200

    Create terve.cs

---
+++ terve.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Terve
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Terve!");
+        }
+    }
+}
+
+//basic helloworld script.
